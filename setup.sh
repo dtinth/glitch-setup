bf4ee6ffcce41b8712b2b46ed40e42382b6957c2
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-15 04:37:19 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ echo meow
 GH_PROJECT=$(/opt/nvm/versions/node/v14/bin/node -e "try{console.log(JSON.parse(process.argv[1]).targetRepo)}catch(e){console.error(e)}" -- "$(curl -L https://raw.githubusercontent.com/dtinth/glitch-synchronizer/master/projects/$PROJECT_DOMAIN.config.json)")
 GH_URL="https://github.com/$GH_PROJECT.git"
 if [ -n "$GH_PROJECT" ]; then git remote add gh "$GH_URL" || git remote set-url gh "$GH_URL"; fi
+if [ -n "$GH_PROJECT" ]; then git pull gh; fi
